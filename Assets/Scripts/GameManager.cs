c00df4e8ed6b6865d8d18c62d48dd960d6a8f265
Matthew Howat <matthewj.howat@gmail.com>
2018-05-02 13:02:00 -0600

    started game manager and loader
    Sets up a call hierarchy and enforces the singleton instance

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public static GameManager instance = null; //Static instance of GameManager which allows it to be accessed by any other script
+    public BoardManager boardScript; //Store a reference to our BoardManager which will set up the level.
+
+    private int level = 3;  //Current level number, expressed in game as "Day 1".
+
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        if (instance == null) //if instance is null use this gamemanager
+            instance = this;
+        else if (instance != this) //If instance already exists and it's not this:
+            Destroy(gameObject);//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+
+
+
+        DontDestroyOnLoad(gameObject); //persists scene to keep track of score
+        boardScript = GetComponent<BoardManager>();        //Get a component reference to the attached BoardManager script
+        InitGame();
+    }
+
+
+    // Use this for initialization
+    void InitGame () {
+        boardScript.SetupScene(level);  //Call the SetupScene function of the BoardManager script, pass it current level number.
+    }
 	
 	// Update is called once per frame
 	void Update () {
