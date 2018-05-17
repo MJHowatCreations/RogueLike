62c923805bd9e55c7ab262e92b4e6aa0f104c6bf
Matthew Howat <matthewj.howat@gmail.com>
2018-05-17 00:48:09 -0600

    player finished

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 //Player inherits from MovingObject, our base class for objects that can move, Enemy also inherits from this.
@@ -28,14 +29,99 @@ public class Player : MovingObject {
         base.Start();
 
     }
-
+    //This function is called when the behaviour becomes disabled or inactive.
     private void OnDisabled()
     {
+        //When Player object is disabled, store the current local food total in the GameManager so it can be re-loaded in next level.
         GameManager.instance.playerFoodPoints = food;
     }
 
     // Update is called once per frame
     void Update () {
+
+        //If it's not the player's turn, exit the function.
+        if (!GameManager.instance.playersTurn) return;
+         
+        int horizontal = 0; //Used to store the horizontal move direction.
+        int vertical = 0; //Used to store the vertical move direction.
+
+
+        //Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
+        horizontal = (int)Input.GetAxisRaw("Horizontal");
+
+        //Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
+        vertical = (int)Input.GetAxisRaw("Vertical");
+
+
+        //Check if moving horizontally, if so set vertical to zero
+        if (horizontal != 0)
+            vertical = 0;
+
+        //Check if we have a non-zero value for horizontal or vertical
+        if (horizontal != 0 || vertical != 0)
+
+            //Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
+            //Pass in horizontal and vertical as parameters to specify the direction to move Player in.
+            AttemptMove<Wall>(horizontal, vertical);
+
+
 		
 	}
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Wall hitwall = component as Wall;
+        hitwall.DamageWall(wallDamage);
+
+        animator.SetTrigger("playerChop");
+    }
+
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        food--;
+        base.AttemptMove<T>(xDir, yDir);
+
+        RaycastHit2D hit;
+        CheckIfGameOver();
+
+        GameManager.instance.playersTurn = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Exit")
+        {
+            Invoke("Restart", restartLevelDelay);
+            enabled = false;
+        }
+        else if (other.tag == "Food")
+        {
+            food += pointsPerFood;
+            other.gameObject.SetActive(false);
+        }
+        else if (other.tag == "Soda")
+        {
+            food += pointsPerSoda;
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoseFood(int loss)
+    {
+        animator.SetTrigger("playerHit");
+        food -= loss;
+        CheckIfGameOver();
+    }
+
+    private void CheckIfGameOver()
+    {
+        if (food <= 0)
+            GameManager.instance.GameOver();
+    }
 }
